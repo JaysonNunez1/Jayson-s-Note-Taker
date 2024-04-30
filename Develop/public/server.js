a482ac8c03469514c38d0824bcd007f609c50109
JaysonNunez1 <jaysonnunez3@gmail.com>
2024-04-29 22:32:35 -0400

    added middleware and required 2 routes

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -5,3 +5,13 @@ const PORT = process.env.PORT || 3001;
 const path = require('path');
 
 const app = express();
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+const apiRoutes = require('./routes/apiRoutes');
+
+const htmlRoutes = require('./routes/htmlRoutes');
