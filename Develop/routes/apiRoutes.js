b6614659dd146f23d8fb026ecca2306916180794
JaysonNunez1 <jaysonnunez3@gmail.com>
2024-05-01 08:41:58 -0400

    handled get for existing notes to be on the page and handled the post new notes

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -4,3 +4,29 @@ const fs = require('fs');
 
 const uuid = require('../helpers/uuid');
 
+router.get('/notes', function(req,res){
+    fs.readFile('./db/db.json', (err,data) =>{
+        if (err) throw err;
+        dbData = JSON.parse(data);
+        res.send(dbData);
+    });
+});
+
+router.post ('/notes', function (req,res){
+    const userNotes = req.body;
+    fs.readFile('./db/db.json', (err,data) =>{
+        if (err) throw err;
+        dbData = JSON.parse(data);
+        dbData.forEach((note, index) =>{
+            note.id = uuid();
+            return dbData;
+        });
+        console.log(dbData);
+
+        stringData = JSON.stringify(dbData);
+        fs.writeFile('./db/db.json', stringData, (err) =>{
+            if (err) throw err;
+        });
+    });
+    res.send('Added');
+});
