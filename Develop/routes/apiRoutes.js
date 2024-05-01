47d79a078b6875102754e77a6965435383caee32
JaysonNunez1 <jaysonnunez3@gmail.com>
2024-05-01 08:55:13 -0400

    handled delete request based on id compared id of deleted notes to notes in the db json file

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -30,3 +30,23 @@ router.post ('/notes', function (req,res){
     });
     res.send('Added');
 });
+
+router.delete('/notes/:id', function(req,res) {
+    const deleteNote = req.params.id;
+    console.log(`Delete note ID: ${deleteNote}`);
+    fs.readFile('./db/db.json', (err,data) => {
+        if (err) throw err;
+        dbData = JSON.parse(data);
+        for (let i = 0; i < dbData.length; i++) {
+            if (dbData[i].id === deleteNote) {
+                dbData.splice([i], 1);
+                stringData = JSON.stringify(dbData);
+                fs.writeFile('./db/db.json', stringData, (err) => {
+                    if (err) throw err;
+                });
+            }};
+            res.status(204).send();
+        });
+});
+
+module.exports = router;
